2f60827920475a5f25324fd5a5d491d38375627c
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-07-12 16:49:14 -0500

    added projects to portfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,55 +1,61 @@
 import React from "react";
-import Portfolio from "./pages/Portfolio";
 import "../styles/pages.css";
 import "../styles/Portfolio.css";
 
 const projects = [
   {
-    projectTitle: "Movie-Geeks-Watchlist",
-    deployedAppLink: "https://shannonkprice00.github.io/movie-watchlist/",
-    imageSrc: "./assets/Movie-Watchlist.png",
+    projectTitle: "Food4Thought",
+    deployedAppLink: "https://floating-journey-48487.herokuapp.com/",
+    imageSrc: "./assets/Food4Thought.png",
+    repoLink: "https://github.com/MVincent15/Food4Thought",
   },
   {
     projectTitle: "Movie-Geeks-Watchlist",
     deployedAppLink: "https://shannonkprice00.github.io/movie-watchlist/",
     imageSrc: "./assets/Movie-Watchlist.png",
+    repoLink: "https://github.com/shannonkprice00/movie-watchlist",
   },
   {
-    projectTitle: "Movie-Geeks-Watchlist",
-    deployedAppLink: "https://shannonkprice00.github.io/movie-watchlist/",
-    imageSrc: "./assets/Movie-Watchlist.png",
+    projectTitle: "J.A.T.E",
+    deployedAppLink: "https://lit-castle-67361-fccb0af3a0da.herokuapp.com/",
+    imageSrc: "./assets/JATE.png",
+    repoLink: "https://github.com/shannonkprice00/JATE-Text-Editor",
   },
   {
-    projectTitle: "Movie-Geeks-Watchlist",
-    deployedAppLink: "https://shannonkprice00.github.io/movie-watchlist/",
-    imageSrc: "./assets/Movie-Watchlist.png",
+    projectTitle: "Weather-Dashboard",
+    deployedAppLink: "https://shannonkprice00.github.io/weather-dashboard/",
+    imageSrc: "./assets/Weather-Dashboard.png",
+    repoLink: "https://github.com/shannonkprice00/weather-dashboard",
   },
   {
-    projectTitle: "Movie-Geeks-Watchlist",
-    deployedAppLink: "https://shannonkprice00.github.io/movie-watchlist/",
-    imageSrc: "./assets/Movie-Watchlist.png",
+    projectTitle: "Work Day Scheduler",
+    deployedAppLink: "https://shannonkprice00.github.io/Calendar-Project/",
+    imageSrc: "./assets/Workday-Planner.png",
+    repoLink: "https://github.com/shannonkprice00/Calendar-Project",
   },
   {
-    projectTitle: "Movie-Geeks-Watchlist",
-    deployedAppLink: "https://shannonkprice00.github.io/movie-watchlist/",
-    imageSrc: "./assets/Movie-Watchlist.png",
+    projectTitle: "Password Generator",
+    deployedAppLink: "https://shannonkprice00.github.io/Password-Generator/",
+    imageSrc: "./assets/Password-Generator.png",
+    repoLink: "https://github.com/shannonkprice00/Password-Generator",
   },
 ];
 
 export default function Project() {
   return (
-    <div className="container-fluid page-container">
+    <div className="container-fluid d-flex page-container">
       <h1>My Portfolio</h1>
       {projects.map((project) => (
         <div className="project">
           <label className="project-label">
-            <h4>{project.projectTitle}</h4>
+            <p>{project.projectTitle}</p>
+            <a href={project.repoLink} target="_blank">GitHub Repo</a>
           </label>
-          <a href={project.deployedAppLink}>
+          <a href={project.deployedAppLink} target="_blank">
             <img
               src={project.imageSrc}
               alt="image of project-1"
-              className="smaller-images"
+              className="project-images"
             />
           </a>
         </div>
